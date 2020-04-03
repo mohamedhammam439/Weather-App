180b07cfb67c6fb9eed6a6af4bf069934f50d701
mohamedhammam439 <mohamedhammam439@gmail.com>
2020-04-03 11:37:24 +0200

    Updates

---
+++ precache-manifest.dfd38f07b699a51741e60e61cb4d16f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0644bfa426f0e9fd9f23baa035bbf07c",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "7ab2886723a1b275eec9",
+    "url": "/Weather-App/static/css/main.e4ea3af6.chunk.css"
+  },
+  {
+    "revision": "6e3e1ea8a1703768ad7b",
+    "url": "/Weather-App/static/js/2.f79569f0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Weather-App/static/js/2.f79569f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab2886723a1b275eec9",
+    "url": "/Weather-App/static/js/main.c0d2af4a.chunk.js"
+  },
+  {
+    "revision": "a2c7ca580732ce8e9737",
+    "url": "/Weather-App/static/js/runtime-main.95892be8.js"
+  },
+  {
+    "revision": "8baf47ae97b1394757b21f7d86472409",
+    "url": "/Weather-App/static/media/free-weather-background-hd-wallpapers-windows-apple-amazing-4k-samsung-wallpapers-free-download-1920x1080.8baf47ae.jpg"
+  }
+]);
